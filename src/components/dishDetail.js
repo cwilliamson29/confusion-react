b4414dd970b8a5fa987faa0416eb65f1d37dc240
Chris Williamson <chriswill0629@gmail.com>
2022-08-15 07:29:58 -0400

    Creating menu

---
+++ src/components/dishDetail.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedDish: null,
+    };
+  }
+
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish });
+  }
+
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <div>
+          <Card>
+            <CardImg width="100%" object src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
