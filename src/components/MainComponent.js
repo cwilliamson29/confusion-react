5f906a2c0e1b90f6e5c1c395c4cc2fab0b30964b
Chris Williamson <chriswill0629@gmail.com>
2022-08-15 23:06:36 -0400

    Setting up additional json files

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Menu from "./menuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./dishDetail";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+import { COMMENTS } from "../shared/comments";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 class Main extends Component {
@@ -13,19 +17,29 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
     return (
       <div>
         <Header />
         <Routes>
-          <Route path="/home" element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
           <Route path="menu" element={<Menu dishes={this.state.dishes} />} />
+          <Route path="contactus" element={<Contact />} />
         </Routes>
 
         <Footer />
